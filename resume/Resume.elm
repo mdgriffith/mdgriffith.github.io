f4767ae80896baa24ad5c5d823c181bfdbcab182
Matthew Griffith <mdg.griffith@gmail.com>
2016-06-02 21:15:52 -0400

    Edits

--- resume/Resume.elm
+++ resume/Resume.elm
@@ -20,11 +20,11 @@ view model =
 Matthew Griffith
 ================
 
-> _I am a programmer with many interests.  I view programming as my tool to dive deeper into the subjects that I love, be it art, music, design, or biology.  My passion is in making things._
+> _I am a programmer with many interests and a passion for making things. I view programming as my tool to dive deeper into the subjects that I love, be it art, music, design, or biology._
 >
 > _I consider myself fluent in `Python`, `Javascript`, `Elm`, and `Haskell`, though with less experience in some of the deeper aspects of Haskell._
 >
-> _This position at Cornell Tech interests me greatly.  I flourish when given creative license, interesting subject matter, and smart people to collaborate with.  Being able to work on varied projects is incredibly intriguing and something I believe I am well suited for._
+> _This position at Cornell Tech interests me greatly.  I flourish when given creative license, interesting subject matter, and smart people to collaborate with.  Being able to work on varied projects intrigues me and is something I believe I am well suited for._
 
 
 <span class="strikethrough">History</span>
